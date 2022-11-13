cada55b8d00d6c9779b254abe03754584c045508
Mikhail Brinchuk <thecentury@gmail.com>
2022-11-13 19:21:48 +0100

    Calculate password hash

--- app/Main.hs
+++ app/Main.hs
@@ -18,14 +18,19 @@ import Network.HTTP.Req
 import Data.Aeson ()
 import Control.Monad.IO.Class ()
 import Data.ByteString as B ( ByteString )
+import qualified Data.ByteString.Char8 as BS (pack, unpack)
 import Data.Text as T ( unpack )
 import Data.Text.Encoding as T ( decodeUtf8 )
 import Data.Text.IO as T ()
 import qualified Network.HTTP.Client as L ( CookieJar )
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString.Base64 as Base64 ( encode )
 
 import Text.HandsomeSoup ( (!), css, parseHtml )
 import Text.XML.HXT.Core ( (>>>), runX )
 import System.Environment (getEnv)
+import Data.ByteString.Builder (toLazyByteString, byteStringHex)
+import Data.ByteString.Lazy (toStrict)
 
 byteStringToString :: ByteString -> String
 byteStringToString = T.unpack . T.decodeUtf8
@@ -35,7 +40,22 @@ requiredMeta key meta = case lookup key meta of
   Just value -> value
   Nothing    -> error $ "Required meta tag not found: " ++ key
 
-visitStartPage :: IO (String, String, L.CookieJar)
+computeHash :: String -> String
+computeHash s =
+  hexStr where
+    bs = BS.pack s
+    hash = SHA256.hash bs
+    hexStr = BS.unpack . toStrict . toLazyByteString . byteStringHex $ hash
+
+encodeBase64 :: String -> String
+encodeBase64 = BS.unpack . Base64.encode . BS.pack
+
+data Tokens = Tokens
+  { csrfToken :: String
+  , csrfParam :: String
+  } deriving (Show)
+
+visitStartPage :: IO (Tokens, L.CookieJar)
 visitStartPage = do
   (response, cookies) <- runReq defaultHttpConfig $ do
     startPageRequest <-
@@ -58,14 +78,14 @@ visitStartPage = do
 
   let csrfToken = requiredMeta "csrf_token" metaMap
   let csrfParam = requiredMeta "csrf_param" metaMap
-
-  pure (csrfToken, csrfParam, cookies)
+  let tokens = Tokens {..}
+  pure (tokens, cookies)
 
 data Config = Config
   { userName :: String
   , password :: String
   , deviceName :: String
-  } deriving (Show, Eq)
+  } deriving (Show)
 
 loadConfig :: IO Config
 loadConfig = do
@@ -74,11 +94,19 @@ loadConfig = do
   deviceName <- getEnv "ROUTER_TARGET_DEVICE"
   pure Config {..}
 
+encodePassword :: Config -> Tokens -> String
+encodePassword Config {..} Tokens {..} =
+  computeHash $ userName ++ (encodeBase64 . computeHash $ password) ++ csrfParam ++ csrfToken
+
 main :: IO ()
 main = do
   cfg <- loadConfig
 
-  (csrfToken, csrfParam, cookie) <- visitStartPage
-  putStrLn $ "csrf_token = " ++ csrfToken
-  putStrLn $ "csrf_param = " ++ csrfParam
+  putStrLn $ "Password hash: " ++ (encodeBase64 . computeHash $ password cfg)
+
+  (tokens, cookie) <- visitStartPage
+  putStrLn $ "csrf_token = " ++ csrfToken tokens
+  putStrLn $ "csrf_param = " ++ csrfParam tokens
+
+  putStrLn $ "Password hash: " ++ encodePassword cfg tokens
   print cookie
